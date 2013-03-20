087762560894ee8c87fb6248c989984667ecff91
root <root@localhost.localdomain>
2013-03-20 13:58:18 +0800

    fix some errors

--- install_centos.sh
+++ install_centos.sh
@@ -1,6 +1,10 @@
+sudo yes | yum install mercurial.i686
 hg clone https://vim.googlecode.com/hg/ vim
 cd vim/src
 make
+sudo make install
+mv /usr/bin/vim /usr/bin/vim_bak
+ln -s /usr/local/bin/vim /usr/bin/vim
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/yenchiugu/my_vim.git ~/.vim
@@ -8,6 +12,5 @@ git clone https://github.com/yenchiugu/my_vim.git ~/.vim
 cp ~/.vimrc ~/vimrcbak
 cp ~/.gvimrc ~/gvimrcbak
 
-
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.vim/.gvimrc ~/.gvimrc
